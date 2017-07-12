8e64f1f6cbd559069d7255d5db022b8b9fa664d9
bastosbf <bastos@gmail.com>
2017-07-12 21:32:35 +0300

    code review formula de bhaskara

--- formula-de-bhaskara/main.c
+++ formula-de-bhaskara/main.c
@@ -2,39 +2,56 @@
 #include <stdlib.h>
 #include <math.h>
 
+int code(int code) {
+	system("PAUSE");
+	return code;
+}
+
+int error() {
+	return code(-1);
+}
+
+int success() {
+	return code(0);
+}
+
 int main(int argc, char *argv[])
 {
-    float a, b, c, delta, x1, x2;
+    float a;
+	float b;
+	float c;
+	float delta;
+	float x1; 
+	float x2;
     
-    printf("\ninsira o valor de a.\n");
+	//ax^2 + bx + c = 0	
+    printf("\nInsira o valor de a: ");
     scanf("%g", &a);
-    printf("\ninsira o valor de b.\n");
+	int tries = 1;
+	while (a == 0) {
+		if(tries == 3) {
+			printf("\n O valor de A nao pode ser igual a zero! Programa encerrado.");
+			return error();
+		}
+		printf("\n O valor de A nao pode ser igual a zero! Por favor digite o valor de a novamente: ");	
+		scanf("%g", &a);
+		tries++;
+	}
+    printf("\nInsira o valor de b: ");
     scanf("%g", &b);
-    printf("\ninsira o valor de c.\n");
+    printf("\nInsira o valor de c: ");
     scanf("%g", &c);
     
-    delta = (b * b) + (-4 * a * c);
-    x1 = (-b + sqrt(delta)) / (2*a);
-    x2 = (-b - sqrt(delta)) / (2*a);
- 
-    if ((delta>=0 ) && (a!=0))
-    {
-    printf("\n Valor de x1 e: %g", x1);
-    
-    printf("\n Valor de x2 e: %g\n", x2);
-    
-    }else{
-          if (a==0)
-          {
-          printf("\n O valor de A nao pode ser igual a zero! Por favor, reinicie o programa.");
-          }else{
-                printf("\n O valor de delta nao pode ser negativo.\n");
-              
-                printf("\n Por favor, reinicie o programa!\n");
-                }
-                }
-                
-  
-  system("PAUSE");	
-  return 0;
+    delta = pow(b,2) -4 * a *c;
+	if (delta < 0) {
+		printf("\n O valor de delta nao pode ser negativo! Programa encerrado.");  
+		return error();
+    } 
+	x1 = (-b + sqrt(delta)) / (2 * a);
+	x2 = (-b - sqrt(delta)) / (2 * a);
+
+	printf("\n Valor de x1 e: %g", x1);    
+	printf("\n Valor de x2 e: %g\n", x2);
+	
+	return success();
 }
